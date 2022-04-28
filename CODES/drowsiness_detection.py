d9263894f652d72ac75f37fb685ab45aa6242105
kufirre <kufy@yourduino.com>
2022-04-28 17:18:33 +0100

    Added folder containing codes

---
+++ CODES/drowsiness_detection.py
@@ -0,0 +1,206 @@
+# USAGE
+# python detect_drowsiness.py --shape-predictor shape_predictor_68_face_landmarks.dat
+# python detect_drowsiness.py --shape-predictor shape_predictor_68_face_landmarks.dat --alarm alarm.wav
+
+# import the necessary packages
+from scipy.spatial import distance as dist
+from imutils.video import VideoStream
+from imutils import face_utils
+from threading import Thread
+import numpy as np
+import playsound
+import argparse
+import imutils
+import time
+import dlib
+import cv2
+import os
+import time, datetime
+import telepot
+from telepot.loop import MessageLoop
+
+now = datetime.datetime.now()
+
+# kuff
+# telegram_bot = telepot.Bot('1784970630:AAEdykEM4-tMa3nTEtWAVq0tpy2QNJqLF2g')
+
+# precious
+# telegram_bot = telepot.Bot('1776422955:AAGX5OJ7Ir-etDJZmPmZ0JdvKl5w5FPYqNM')
+
+# mrs. uche's telegram bot credentials
+telegram_bot = telepot.Bot('1717921994:AAHcePh7uQE4QA36qrFJv6FBLckduGb-PYE')
+print(telegram_bot.getMe())
+
+
+def handle(msg):
+    chat_id = msg['chat']['id']  # Receiving the message from telegram
+    command = msg['text']  # Getting text from the message
+
+    print('Received:')
+    print(command)
+
+    if command == '/detect':
+        while True:
+            if ALARM_ON is True:
+                # capture image and save it
+                cv2.imwrite('/home/pi/codes/drowsy_driver.png', frame)
+
+                # send message to the telegram bot from the raspberry pi
+                telegram_bot.sendMessage(chat_id, str("Driver may be drowsy or sleeping at this time"))
+
+                # send the captured photo of the presumably drowsy or sleeping person
+                telegram_bot.sendPhoto(chat_id, photo=open('/home/pi/codes/drowsy_driver.png', 'rb'))
+
+
+# listen for messages from telegram bot
+MessageLoop(telegram_bot, handle).run_as_thread()
+
+
+def sound_alarm():
+    # play an alarm sound
+    os.system('aplay /home/pi/codes/alarm.wav &')
+    time.sleep(2)
+
+    # play a voice alert for the driver
+    os.system('pico2wave -l en-US -w /home/pi/codes/alert.wav "wake up!" && aplay /home/pi/codes/alert.wav && sudo rm /home/pi/codes/alert.wav')
+
+
+def eye_aspect_ratio(eye):
+    # compute the euclidean distances between the two sets of
+    # vertical eye landmarks (x, y)-coordinates
+    a = dist.euclidean(eye[1], eye[5])
+    b = dist.euclidean(eye[2], eye[4])
+
+    # compute the euclidean distance between the horizontal
+    # eye landmark (x, y)-coordinates
+    c = dist.euclidean(eye[0], eye[3])
+
+    # compute the eye aspect ratio
+    ear = (a + b) / (2.0 * c)
+
+    # return the eye aspect ratio
+    return ear
+
+
+# God will fight
+
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--shape-predictor", required=True, help="path to facial landmark predictor")
+ap.add_argument("-a", "--alarm", type=str, default="", help="path alarm .WAV file")
+ap.add_argument("-w", "--webcam", type=int, default=0, help="index of webcam on system")
+args = vars(ap.parse_args())
+
+# define two constants, one for the eye aspect ratio to indicate
+# blink and then a second constant for the number of consecutive
+# frames the eye must be below the threshold for to set off the
+# alarm
+EYE_AR_THRESH = 0.3
+EYE_AR_CONSEC_FRAMES = 4
+
+# initialize the frame counter as well as a boolean used to
+# indicate if the alarm is going off
+COUNTER = 0
+ALARM_ON = False
+
+# initialize dlib's face detector (HOG-based) and then create
+# the facial landmark predictor
+print("[INFO] loading facial landmark predictor...")
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(args["shape_predictor"])
+
+# grab the indexes of the facial landmarks for the left and
+# right eye, respectively
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+# start the video stream thread
+print("[INFO] starting video stream thread...")
+vs = VideoStream(src=args["webcam"]).start()
+time.sleep(1.0)
+os.system(
+    'pico2wave -l en-US -w start.wav "Drowsiness Detection System STARTED!" && aplay start.wav && sudo rm start.wav')
+
+# loop over frames from the video stream
+while True:
+    # grab the frame from the threaded video file stream, resize
+    # it, and convert it to grayscale
+    # channels)
+    frame = vs.read()
+    frame = imutils.resize(frame, width=450)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # detect faces in the grayscale frame
+    rects = detector(gray, 0)
+
+    # loop over the face detections
+    for rect in rects:
+        # determine the facial landmarks for the face region, then
+        # convert the facial landmark (x, y)-coordinates to a NumPy
+        # array
+        shape = predictor(gray, rect)
+        shape = face_utils.shape_to_np(shape)
+
+        # extract the left and right eye coordinates, then use the
+        # coordinates to compute the eye aspect ratio for both eyes
+        leftEye = shape[lStart:lEnd]
+        rightEye = shape[rStart:rEnd]
+        leftEAR = eye_aspect_ratio(leftEye)
+        rightEAR = eye_aspect_ratio(rightEye)
+
+        # average the eye aspect ratio together for both eyes
+        ear = (leftEAR + rightEAR) / 2.0
+
+        # compute the convex hull for the left and right eye, then
+        # visualize each of the eyes
+        leftEyeHull = cv2.convexHull(leftEye)
+        rightEyeHull = cv2.convexHull(rightEye)
+        cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+        cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+
+        # check to see if the eye aspect ratio is below the blink
+        # threshold, and if so, increment the blink frame counter
+        if ear < EYE_AR_THRESH:
+            COUNTER += 1
+
+            # if the eyes were closed for a sufficient number of
+            # then sound the alarm
+            if COUNTER >= EYE_AR_CONSEC_FRAMES:
+                # if the alarm is not on, turn it on
+                if not ALARM_ON:
+                    ALARM_ON = True
+
+                    # check to see if an alarm file was supplied,
+                    # and if so, start a thread to have the alarm
+                    # sound played in the background
+                    if args["alarm"] != "":
+                        t = Thread(target=sound_alarm)
+                        t.deamon = True
+                        t.start()
+
+                # draw an alarm on the frame
+                cv2.putText(frame, "DROWSINESS ALERT!", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+        # otherwise, the eye aspect ratio is not below the blink
+        # threshold, so reset the counter and alarm
+        else:
+            COUNTER = 0
+            ALARM_ON = False
+
+        # draw the computed eye aspect ratio on the frame to help
+        # with debugging and setting the correct eye aspect ratio
+        # thresholds and frame counters
+        cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+    # show the frame
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        break
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
